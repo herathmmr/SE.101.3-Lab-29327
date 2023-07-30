510d6d071a33ea76659c0ccacaf3d1849ff18b14
herathmmr <140955461+herathmmr@users.noreply.github.com>
2023-07-31 00:24:02 +0530

    Add files via upload

---
+++ lab3/newQuestion2/src/main/java/com/mycompany/newquestion2/NewQuestion2.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.newquestion2;
+
+/**
+ *
+ * @author Malshan
+ */
+public class NewQuestion2 {
+
+    public static void main(String[] args) {
+         encpsulation e1=new encpsulation("malshan", 22, 85000.00f);
+        e1.display();
+    }
+}
